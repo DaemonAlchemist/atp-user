eb525379e87048a754f70d66f197df51a2c6bf62
Andrew Wittrock <andy@wittrock.us>
2015-05-12 10:29:43 -0500

    Add user table installer

--- config/module.config.php
+++ config/module.config.php
@@ -1,49 +1,15 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 return array(
+	'modules' => array(
+		'ATPUser' => array(
+			'version' => '0.9.0',
+		),
+	),
 	'user' => array(
 		'user_model' => 'ATPUser\Model\User',
 		'loginRedirectRoute' => 'home',
 		'logoutRedirectRoute' => 'home',
-	),
-	'admin' => array(
-		'models' =>array(
-			'user_user' => array(
-				'displayName' => 'User',
-				'class' => 'ATPUser\Model\User',
-				'category' => 'Admin',
-				'displayColumns' => array('Username', 'Email'),
-				'defaultOrder' => 'username ASC',
-				'fields' => array(
-					'Username' => array(
-						'type' => 'Text',
-						'label' => 'Username'
-					),
-					'Email' => array(
-						'type' => 'Text',
-						'label' => 'Email',
-					),
-					'Password' => array(
-						'type' => 'Password',
-						'label' => 'Password',
-					),
-				),
-			),
-		),
-	),
-	'asset_manager' => array(
-		'resolver_configs' => array(
-			'paths' => array(
-				__DIR__ . '/../public',
-			),
-		),
 	),
     'router' => array(
         'routes' => array(
--- src/ATPUser/Module.php
+++ src/ATPUser/Module.php
@@ -6,4 +6,20 @@ class Module extends \ATP\Module
 {
 	protected $_moduleName = "ATPUser";
 	protected $_moduleBaseDir = __DIR__;
+	
+	public function getInstallDbQueries()
+	{
+		return array(
+			"CREATE TABLE `atp`.`atpusers_users` (
+				`id` INT(11) NOT NULL AUTO_INCREMENT,
+				`username` CHAR(32) NULL,
+				`password` CHAR(32) NULL,
+				`real_name` CHAR(255) NULL,
+				`initials` CHAR(4) NULL,
+				`email` CHAR(64) NULL,
+				PRIMARY KEY (`id`),
+				UNIQUE INDEX `username_idx` (`username` ASC)
+			)",
+		);
+	}
 }
